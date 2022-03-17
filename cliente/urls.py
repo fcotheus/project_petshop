a3cee603e01e9ff02588953ae6473efe80a29e14
Matheus <85880786+FranciscMatheus@users.noreply.github.com>
2022-03-17 17:15:07 -0300

    feat: Criei o app, home e usuario

--- cliente/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-URLPatterns = [
-    path('', views.cliente, name='cliente')
-]
--- petshop/urls.py
+++ petshop/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cliente.urls')),
+    path('', include('home.urls')),
+    # path('usuario/', include('usuario.urls')),
+    # path('pet/', include('pet.urls')),
+    # path('cliente/', include('cliente.urls')),
+    # path('servicos/', include('servicos.urls')),
+    
 ]

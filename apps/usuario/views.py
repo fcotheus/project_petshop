f2632e930954d8db72f3885ad5faa898cd844ebc
jpcontabilidade <58527993+jpcontabilidade@users.noreply.github.com>
2022-03-26 14:40:57 -0300

    atualizacao teste
    tela

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 def cadastro(request):
     return render(request,'usuario/cadastro_usuario.html')
 
+
 def login(request):
     return render(request,'usuario/login.html')

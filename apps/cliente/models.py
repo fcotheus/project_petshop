f26a4d36de1a440398790c2d242dc6913ae5e1a1
Matheus <85880786+FranciscMatheus@users.noreply.github.com>
2022-04-07 17:07:12 -0300

    feat: cadastro de usuario

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from statistics import mode
 
-# Create your models here.
+class Pessoa(models.Model):
+    UF_CHOICES = (
+        ('AC','AC'),
+        ('AL','AL'),
+        ('AP','AP'),
+        ('AM','AM'),
+        ('BA','BA'),
+        ('CE','CE'),
+        ('DF','DF'),
+        ('ES','ES'),
+        ('GO','GO'),
+        ('MA','MA'),
+        ('MT','MT'),
+        ('MS','MS'),
+        ('MG','MG'),
+        ('PA','PA'),
+        ('PB','PB'),
+        ('PR','PR'),
+        ('PE','PE'),
+        ('PI','PI'),
+        ('RJ','RJ'),
+        ('RN','RN'),
+        ('RS','RS'),
+        ('RO','RO'),
+        ('RR','RR'),
+        ('SC','SC'),
+        ('SP','SP'),
+        ('SE','SE'),
+        ('TO','TO'),
+        )
+
+    TIPO_CLIENTE = (
+    ('ATIVO','Ativo'),
+    ('INATIVO','Inativo'),
+    ('PROSPECCAO','Prospecção'),
+    )
+
+
+    name = models.CharField(max_length=100, default='')
+    cpf = models.CharField(max_length=20, blank=True)
+    rg = models.CharField(max_length=20, blank=True, default='')
+    data_nasc = models.DateField(blank=True)
+    telephone = models.CharField(max_length=20, default='')
+    email = models.EmailField()
+    cep= models.CharField(max_length=15,blank=True, default='')
+    logadouro = models.CharField(max_length=100,blank=True, default='')
+    numero = models.CharField(max_length=20, blank=True, default='')
+    bairro = models.CharField(max_length=50,blank=True, default='')
+    complemento = models.CharField(max_length=50,blank=True, default='')
+    uf = models.TextField(choices=UF_CHOICES,max_length=2, default=0)
+    city = models.CharField(max_length=200,blank=True)
+    tipo_cliente = models.TextField(choices=TIPO_CLIENTE,max_length=15, default=0)
+    data_cricao = models.DateField()
+
+    def __str__(self):
+        return str(self.name)
--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -1,12 +1,75 @@
-from django.shortcuts import render
-from cliente.forms import LeadsForms, ClienteForms
+from ast import Try
+from django.shortcuts import render, redirect
+from apps.usuario.views import cadastro_usuario
+from cliente.models  import Pessoa
+from django.contrib import messages
+from tempus_dominus.widgets import DatePicker
+from datetime import datetime
 
 def cadastro(request):
-    cliente_form = ClienteForms()
-    contexto = {'cliente_form':cliente_form}
-    return render(request, 'cliente/cadastro_pessoa.html',contexto)
+    if request.method == 'POST':
+        name = request.POST['name']
+        cpf = request.POST['cpf']
+        rg = request.POST['rg']
+        data_nasc = request.POST['data_nasc']
+        telephone = request.POST['telephone']
+        email = request.POST['email']
+        cep = request.POST['cep']
+        logadouro = request.POST['logadouro']
+        numero = request.POST['numero']
+        bairro = request.POST['bairro']
+        complemento = request.POST['complemento']
+        uf = request.POST['uf']
+        tipo_cliente = request.POST['tipo_cliente']
+        data_cricao = request.POST['data_cricao']
+
+        try:
+            cadastro_cliente = Pessoa()
+            cadastro_cliente.name = name
+            cadastro_cliente.cpf = cpf
+            cadastro_cliente.rg = rg
+            cadastro_cliente.data_nasc = data_nasc
+            cadastro_cliente.telephone = telephone
+            cadastro_cliente.email = email
+            cadastro_cliente.cep = cep
+            cadastro_cliente.logadouro = logadouro
+            cadastro_cliente.numero = numero
+            cadastro_cliente.bairro = bairro
+            cadastro_cliente.complemento = complemento
+            cadastro_cliente.uf = uf
+            cadastro_cliente.tipo_cliente = tipo_cliente
+            cadastro_cliente.data_cricao =  datetime.now()
+            print(cadastro_cliente.data_cricao)
+            cadastro_cliente.save()
+            return redirect('dashboard')
+        except Exception as error:
+            print(error) 
+            messages.error(request, 'Ocorreu algum erro enviar o formulário!')   
+    return render(request, 'cliente/cadastro_pessoa.html' )
 
 def Lead(request):
-    lead_form = LeadsForms()
-    contexto = {'lead_form':lead_form}
-    return render(request,'cliente/fale_conosco.html',contexto)    
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        telephone = request.POST['telephone']
+        city = request.POST['city']
+        cep = request.POST['cep']
+
+        try:
+            lead = Pessoa()
+            lead.name = name
+            lead.email = email
+            lead.telephone = telephone
+            lead.city = city
+            lead.cep = cep
+            lead.save()
+            messages.success(request, 'Formulário enviado com sucesso!')
+        except Exception as error:
+            print(error)
+            messages.error(request, 'Ocorreu algum erro enviar o formulário!')
+
+        return redirect('/cliente/lead')
+    
+    return render(request, 'cliente/fale_conosco.html')    
+  
+
